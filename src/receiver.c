cee8020c8adca0956e6b0f454ea990ccc8644d54
smokeythekat <owr5273@gmail.com>
2021-12-28 03:38:05 -0400

    actually fixed it

--- src/receiver.c
+++ src/receiver.c
@@ -10,18 +10,32 @@ int receiver_receive(int port, char* filename)
 	struct dsocket_tcp_server server = make_dsocket_tcp_server(port);
 
 	if (dsocket_tcp_server_bind(&server) != 0)
+	{
+		close(server.dscr);
 		return 1;
+	}
 	if (dsocket_tcp_server_start_listen(&server) != 0)
+	{
+		close(server.dscr);
 		return 1;
+	}
 
 	int client = dsocket_tcp_server_listen(&server);
 
 	FILE* fp = fopen(filename, "w");
 	if (fp == 0)
+	{
+		fclose(fp);
+		close(server.dscr);
 		return 1;
+	}
 
 	if (dsocket_tcp_server_send(server, client, "send ready", 10) <= 0)
+	{
+		close(server.dscr);
+		fclose(fp);
 		return 1;
+	}
 
 	char buffer[4096];
 
